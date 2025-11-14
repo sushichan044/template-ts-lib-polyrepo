5169ec1e548645003773b1535f769150d73d6559
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-11-14 22:19:17 +0900

    tsgo

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  attw: { profile: "esmOnly" },
+  attw: { level: "error", profile: "esmOnly" },
   clean: true,
-  dts: true,
+  dts: {
+    tsgo: true,
+  },
   entry: [],
   fixedExtension: true,
   format: "esm",
